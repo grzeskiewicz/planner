6e79828d2c5f029b7e2b2bc8a895dd3445e04219
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2023-02-05 23:35:09 +0100

    Docker

--- src/Crop.js
+++ src/Crop.js
@@ -207,7 +207,7 @@ class Crop extends React.Component {
           <FontAwesomeIcon icon={faTrashAlt} size="lg" />
         </td>
         {crop.scheduled===1?<td>&#10004;</td>:<td onClick={() => this.scheduleWatering(crop)}>[Schedule]</td>}
-        {crop.completed===1 && crop.scheduled===0?<td>&#10004;</td>:<td onClick={() => this.deleteSchedule(crop)}>[Finish]</td>}
+        {crop.completed===1 && crop.scheduled===1?<td>&#10004;</td>:<td onClick={() => this.deleteSchedule(crop)}>[Finish]</td>}
       </tr>
     );
   }
