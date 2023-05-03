37fe077e21b18e47ddf5e07bf62e231335886237
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2023-05-03 11:31:53 +0200

    Docker

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Microgreens from './Microgreens';
 import Crops from './Crops';
 import WeekView from './WeekView';
+import Devices from './Devices';
 import { API_URL, request } from './APIConnection';
 import MonthView from './MonthView';
 import DayView from './DayView';
@@ -22,6 +23,7 @@ class App extends React.Component {
       this.showAddCropTab=this.showAddCropTab.bind(this);
       this.showWeekTab=this.showWeekTab.bind(this);
       this.showMonthTab=this.showMonthTab.bind(this);
+      this.showDevicesTab=this.showDevicesTab.bind(this);
       this.setSelectedDay=this.setSelectedDay.bind(this);
       this.getCrops=this.getCrops.bind(this);
       this.getMicrogreens=this.getMicrogreens.bind(this);
@@ -78,6 +80,10 @@ showMonthTab(){
   this.setState({tab:2});
 }
 
+showDevicesTab(){
+  this.setState({tab:4});
+}
+
 setSelectedDay(day){
   console.log(day);
   this.setState({selectedDay:day});
@@ -95,12 +101,15 @@ render(){
 <div id="crops-week-tab" onClick={this.showWeekTab}><p>Widok [Tydzień]</p></div>
 <div id="crops-month-tab" onClick={this.showMonthTab}><p>Widok [Miesiąc]</p></div>
 <div id="microgreens-tab" onClick={this.showMicrogreensTab}><p>Microgreens</p></div>
+<div id="microgreens-tab" onClick={this.showDevicesTab}><p>Menedżer urządzeń</p></div>
       </div>
       <div id="board">
 {this.state.microgreens !=='' && this.state.tab===0 ? <Crops microgreens={this.state.microgreens} crops={this.state.crops} shelves={this.state.shelves} refreshCrops={this.getCrops} markedCrop={this.state.markedCrop}></Crops>: null}
 {this.state.crops !=='' && this.state.tab===1 ? <WeekView shelves={this.state.shelves} microgreens={this.state.microgreens} crops={this.state.crops} setSelectedDay={this.setSelectedDay} setSelectedCrop={this.setSelectedCrop}></WeekView>:null}
 {this.state.crops !=='' && this.state.tab===2? <MonthView shelves={this.state.shelves} microgreens={this.state.microgreens} crops={this.state.crops} setSelectedDay={this.setSelectedDay} setSelectedCrop={this.setSelectedCrop}></MonthView>:null}
 {this.state.microgreens !=='' && this.state.tab===3 ? <Microgreens microgreens={this.state.microgreens} refreshMicrogreens={this.getMicrogreens}></Microgreens> : null}
+{this.state.tab===3 ? <Devices></Devices> : null}
+
 {this.state.crops !=='' && this.state.selectedDay ? <DayView selectedDay={this.state.selectedDay} setSelectedDay={this.setSelectedDay} crops={this.state.crops} microgreens={this.state.microgreens}></DayView>:''}
 </div>
     </div>
---
+++ src/Devices.js
@@ -0,0 +1,70 @@
+import './Devices.css';
+import React from 'react';
+import { API_URL, request } from "./APIConnection";
+
+
+
+class Devices extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+      }
+}
+
+componentDidMount(){
+
+}
+
+getPumpStatus(rack){
+    fetch(request(`${API_URL}/pumpstatus`, "POST", {"rack_id": rack}))
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result);
+      if (result.success) {
+
+      } else {
+
+      }
+    })
+    .catch((error) => Promise.reject(new Error(error)));
+}
+
+getControllerState(){
+    fetch(request(`http://watering.farmabracia.ovh:3051`, "GET"))
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => Promise.reject(new Error(error)));
+}
+
+getLampsState(rack){
+    fetch(request(`${API_URL}/lampsstate`, "POST", {"rack_id": rack}))
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result);
+      if (result.success) {
+        
+      } else {
+
+      }
+    })
+    .catch((error) => Promise.reject(new Error(error)));
+}
+
+//getLampState(shelf){}
+
+//getFanState(shelf){}
+
+
+
+render(){
+
+  return (
+<div>
+    <button onClick={this.getControllerState}>TEST ORANGE PI</button>
+</div>);}
+}
+
+
+export default Devices;
