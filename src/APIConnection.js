7dfa5327cd9a9be7455512ca3967ab0c14feef83
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2023-07-02 23:37:31 +0200

    Docker

--- src/APIConnection.js
+++ src/APIConnection.js
@@ -1,4 +1,4 @@
-export const API_URL='/api';
+export const API_URL='http://localhost:3001';
 export const headers = new Headers({
     'Accept': 'application/json',
     'Content-Type': 'application/json',
