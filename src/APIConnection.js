350b1711f4b844baa479771cd966fcd8a517f277
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2023-07-04 17:44:58 +0200

    Server

--- src/APIConnection.js
+++ src/APIConnection.js
@@ -1,4 +1,4 @@
-export const API_URL='http://localhost:3001';
+export const API_URL='';
 export const headers = new Headers({
     'Accept': 'application/json',
     'Content-Type': 'application/json',
