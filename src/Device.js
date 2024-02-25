7aa388599ad03b45c53106d04431d923d0d57fcb
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2024-02-26 00:01:31 +0100

    Added reset device

--- src/Device.js
+++ src/Device.js
@@ -1,7 +1,6 @@
 import './Devices.css';
 import React from 'react';
 import {pingCheck,request,request2} from "./APIConnection";
-import axios from 'axios';
 const RACK_URL='192.168.2.5'
 
 
@@ -23,53 +22,27 @@ async checkStatus(){
     this.setState({status:await pingCheck(RACK_URL,this.props.port)});
 }
 
-resetDevice(ip,port){
-  axios.get(`http://${ip}/cm?cmnd=Power%20off`)
-    .then((response) => {
-console.log(response);
-setTimeout(() => {
-  axios.get(`http://${ip}/cm?cmnd=Power%20on`)
-  .then((result2) => {
-      this.checkStatus();
-  }).catch((error2) => {
-    console.log(error2);
-  });
-}, 5000);
-
-    }).catch((error) => {
-      console.log(error);
-    });
+resetDevice(){
+  fetch(request(`${API_URL}/resetorangepi`, 'GET'))
+    .then(res => res.json())
+    .then(result => { 
+      if (result.success=="true") {
+        setTimeout(() => this.checkStatus, 60);
+      } else{
+alert("Nie można zresetować OrangePI");
+      }
+    }).catch(error => Promise.reject(new Error(error))); 
   }
 
- /* console.log(`http://${ip}/cm?cmnd=Power%20off`);
-    fetch(request2(`http://${ip}/cm?cmnd=Power%20off`, "GET"))
-  //.then((res) => res.json())
-  .then((result) => {
-    console.log(result)
-    }).catch((error) => Promise.reject(new Error(error)));
-}*/
-
-/*
-    if (result.POWER == "OFF") {
-    setTimeout(() => {
-        fetch(request2(`http://${ip}/cm?cmnd=Power%20on`, "GET"))
-        .then((res) => res.json())
-        .then((result2) => {
-            this.checkStatus();
-        })
-      }, 5000);
-    }
-*/
 
 
 
 
 render(){
     //const port=this.props.socketPort!=='' ? this.props.socketPort : this.props.port;
-    const socketIP=this.props.socketIP;
   return (
 <div className='Device'>
-   <p>{this.props.name}</p><p>PORT: {this.props.port}</p><p>Status: {this.state.status}</p>{this.state.status!=="active"? <button onClick={()=>this.resetDevice(socketIP)}>Reset</button>:''}
+   <p>{this.props.name}</p><p>PORT: {this.props.port}</p><p>Status: {this.state.status}</p>{this.state.status!=="active"? <button onClick={()=>this.resetDevice()}>Reset</button>:''}
 </div>);}
 }
 
