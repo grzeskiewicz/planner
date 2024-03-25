5012e0fc1920a270ec7bfbbf8350a1efb58efda3
Ariel Grze <ariel.grzeskiewicz@gmail.com>
2024-03-25 22:36:45 +0100

    Changed orangepi ip

--- src/Device.js
+++ src/Device.js
@@ -1,7 +1,7 @@
 import './Devices.css';
 import React from 'react';
 import {pingCheck,request,API_URL} from "./APIConnection";
-const RACK_URL='192.168.2.5'
+const RACK_URL='192.168.2.6'
 
 class Device extends React.Component {
   constructor(props) {
@@ -121,20 +121,7 @@ this.setState({info:result.data.Status});
     .catch((error) => Promise.reject(new Error(error)));
   }
 
-/*
-        <option value="5">5</option>
-         <option value="6">6</option>
-         <option value="7">7</option>
-         <option value="8">8</option>
-         <option value="9">9</option>
-         <option value="10">10</option>
-         <option value="11">11</option>
-         <option value="12">12</option>
-         <option value="13">13</option>
-         <option value="14">14</option>
-         <option value="15">15</option>
-         <option value="16">16</option>
-*/
+
 
 render(){
     //const port=this.props.socketPort!=='' ? this.props.socketPort : this.props.port;
@@ -156,6 +143,18 @@ render(){
          <option value="2">2</option>
          <option value="3">3</option>
          <option value="4">4</option>
+         <option value="5">5</option>
+         <option value="6">6</option>
+         <option value="7">7</option>
+         <option value="8">8</option>
+         <option value="9">9</option>
+         <option value="10">10</option>
+         <option value="11">11</option>
+         <option value="12">12</option>
+         <option value="13">13</option>
+         <option value="14">14</option>
+         <option value="15">15</option>
+         <option value="16">16</option>
          </select>
          <p>Czas otwarcia [s]:</p>
          <input type="number" value={this.state.duration} onChange={this.handleDuration}></input>
