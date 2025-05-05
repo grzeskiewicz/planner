56eeca2b54b435dfbbf806c34a81c6e41cc245b0
Grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2025-05-05 14:37:47 +0200

    Fixed addMicrogreens bindings

--- src/AddMicrogreens.js
+++ src/AddMicrogreens.js
@@ -28,23 +28,22 @@ class AddMicrogreens extends React.Component {
     this.handleBlackout = this.handleBlackout.bind(this);
     this.handleLight = this.handleLight.bind(this);
     this.handleColor = this.handleColor.bind(this);
-    this.saveMicrogreen = this.saveMicrogreen.bind(this);
+    this.addMicrogreens = this.addMicrogreens.bind(this);
 
   }
 
   addMicrogreens(event) {
     event.preventDefault();
     const microgreensData = JSON.parse(JSON.stringify(this.state));
-    delete microgreensData.showAMF;
-    delete microgreensData.selectedMicrogreens;
 
     fetch(request(`${API_URL}/addmicrogreens`, "POST", microgreensData))
       .then((res) => res.json())
       .then((result) => {
         console.log(result);
         if (result.success) {
-          this.props.refreshMicrogreens();
           alert("Dodano definicję mikroliści.");
+          this.props.showMicrogreensTab();
+          this.props.refreshMicrogreens();
         } else {
           alert("SQL Error - powtarzające się nazwy lub błędne wartości!")
         }
@@ -52,42 +51,41 @@ class AddMicrogreens extends React.Component {
       .catch((error) => { alert("Błąd! Nie udało się dodać definicji mikroliści!"); return error });
   }
 
-  handleNameEN(event) {
-    this.setState({ nameEN: event.target.value });
+  handleNamePL(e) {
+    this.setState({ namePL: e.target.value });
   }
 
-  handleNamePL(event) {
-    this.setState({ namePL: event.target.value });
-  }
-  handleGramsTray(event) {
-    this.setState({ gramsTray: event.target.value });
+  handleNameEN(e) {
+    this.setState({ nameEN: e.target.value });
   }
 
-  handleGramsHarvest(event) {
-    this.setState({ gramsHarvest: event.target.value });
-
-  }
-  handleWateringLevel(event) {
-    this.setState({ wateringLevel: event.target.value });
+  handleGramsTray(e) {
+    this.setState({ gramsTray: e.target.value });
   }
 
-
-  handleWeighting(event) {
-    this.setState({ weight: event.target.value });
+  handleGramsHarvest(e) {
+    this.setState({ gramsHarvest: e.target.value });
+  }
+  handleWateringLevel(e) {
+    this.setState({ wateringLevel: e.target.value });
   }
 
-  handleBlackout(event) {
-    this.setState({ blackout: event.target.value });
+  handleWeight(e) {
+    this.setState({ weight: e.target.value });
   }
 
-  handleExposure(event) {
-    this.setState({ light: event.target.value });
+  handleBlackout(e) {
+    this.setState({ blackout: e.target.value });
   }
 
-  handleColor(event) {
-    console.log(event.target.value);
-    this.setState({ color: event.target.value });
+  handleLight(e) {
+    this.setState({ light: e.target.value });
   }
+
+  handleColor(e) {
+    this.setState({ color: e.target.value });
+  }
+
 
 
   render() {
@@ -101,9 +99,9 @@ class AddMicrogreens extends React.Component {
         <option value={2}>POZIOM NAWODNIENIA - 2</option>
         <option value={3}>POZIOM NAWODNIENIA - 3</option>
       </select>
-      <input type="number" max="30" placeholder={isMobile ? 'Obciąż.' : 'Obciążanie [dni]'} value={this.state.weight} onChange={this.handleWeighting} required pattern="^\d+$" title="Wprowadź liczbę"></input>
+      <input type="number" max="30" placeholder={isMobile ? 'Obciąż.' : 'Obciążanie [dni]'} value={this.state.weight} onChange={this.handleWeight} required pattern="^\d+$" title="Wprowadź liczbę"></input>
       <input type="number" max="30" placeholder={isMobile ? 'Black.' : 'Blackout [dni]'} value={this.state.blackout} onChange={this.handleBlackout} required pattern="^\d+$" title="Wprowadź liczbę"></input>
-      <input type="number" max="30" placeholder={isMobile ? 'Naśw.' : 'Naświetlanie [dni]'} value={this.state.light} onChange={this.handleExposure} required pattern="^\d+$" title="Wprowadź liczbę"></input>
+      <input type="number" max="30" placeholder={isMobile ? 'Naśw.' : 'Naświetlanie [dni]'} value={this.state.light} onChange={this.handleLight} required pattern="^\d+$" title="Wprowadź liczbę"></input>
       <input id="microgreens-color-picker" placeholder='Kolor' value={this.state.color} onChange={this.handleColor} type="color"></input>
       <button type='submit'>DODAJ</button>
       {this.state.error !== '' ? <p className="error">{this.state.error}</p> : ''}
