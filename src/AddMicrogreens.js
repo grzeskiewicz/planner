2ebffbc50f4e924e7332e2ad903ad41d6a221604
Grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2025-05-05 13:53:11 +0200

    Fixed addMicrogreens bindings

--- src/AddMicrogreens.js
+++ src/AddMicrogreens.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {request,API_URL} from "./APIConnection";
+import { request, API_URL } from "./APIConnection";
 import { isMobile } from 'react-device-detect';
 import moment from 'moment';
 import Microgreens from './Microgreens';
@@ -7,26 +7,36 @@ import './addMicrogreens.css';
 
 class AddMicrogreens extends React.Component {
   constructor(props) {
-      super(props);
-      this.state = {
-        nameEN: '',
-        namePL: '',
-        gramsTray: '',
-        gramsHarvest:'',
-        wateringLevel:'1',
-        weight: '',
-        blackout: '',
-        light: '',
-        color: '#000000'
-      }
+    super(props);
+    this.state = {
+      nameEN: '',
+      namePL: '',
+      gramsTray: '',
+      gramsHarvest: '',
+      wateringLevel: '1',
+      weight: '',
+      blackout: '',
+      light: '',
+      color: '#000000'
+    }
+    this.handleNamePL = this.handleNamePL.bind(this);
+    this.handleNameEN = this.handleNameEN.bind(this);
+    this.handleGramsTray = this.handleGramsTray.bind(this);
+    this.handleGramsHarvest = this.handleGramsHarvest.bind(this);
+    this.handleWateringLevel = this.handleWateringLevel.bind(this);
+    this.handleWeight = this.handleWeight.bind(this);
+    this.handleBlackout = this.handleBlackout.bind(this);
+    this.handleLight = this.handleLight.bind(this);
+    this.handleColor = this.handleColor.bind(this);
+    this.saveMicrogreen = this.saveMicrogreen.bind(this);
 
-}
+  }
 
   addMicrogreens(event) {
     event.preventDefault();
-   const microgreensData = JSON.parse(JSON.stringify(this.state));
-   delete microgreensData.showAMF;
-   delete microgreensData.selectedMicrogreens;
+    const microgreensData = JSON.parse(JSON.stringify(this.state));
+    delete microgreensData.showAMF;
+    delete microgreensData.selectedMicrogreens;
 
     fetch(request(`${API_URL}/addmicrogreens`, "POST", microgreensData))
       .then((res) => res.json())
@@ -39,10 +49,10 @@ class AddMicrogreens extends React.Component {
           alert("SQL Error - powtarzające się nazwy lub błędne wartości!")
         }
       })
-      .catch((error) => {alert("Błąd! Nie udało się dodać definicji mikroliści!"); return error});
+      .catch((error) => { alert("Błąd! Nie udało się dodać definicji mikroliści!"); return error });
   }
 
-handleNameEN(event) {
+  handleNameEN(event) {
     this.setState({ nameEN: event.target.value });
   }
 
@@ -53,7 +63,7 @@ handleNameEN(event) {
     this.setState({ gramsTray: event.target.value });
   }
 
-  handleGramsHarvest(event){
+  handleGramsHarvest(event) {
     this.setState({ gramsHarvest: event.target.value });
 
   }
@@ -80,16 +90,16 @@ handleNameEN(event) {
   }
 
 
-render(){
+  render() {
     const addMicrogreensForm = <form className="addMicrogreensForm" onSubmit={this.addMicrogreens}>
       <input type="text" placeholder='Nazwa (ENG)' value={this.state.nameEN} onChange={this.handleNameEN} required></input>
       <input type="text" placeholder='Nazwa (PL)' value={this.state.namePL} onChange={this.handleNamePL} required></input>
       <input type="number" placeholder='Taca waga [g]' max="2000" value={this.state.gramsTray} onChange={this.handleGramsTray} required pattern="^\d+$" title="Wprowadź liczbę"></input>
       <input type="number" placeholder='Zbiór waga L taca [g]' max="2000" value={this.state.gramsHarvest} onChange={this.handleGramsHarvest} required pattern="^\d+$" title="Wprowadź liczbę"></input>
       <select value={this.state.wateringLevel} onChange={this.handleWateringLevel} required>
-    <option value={1}>POZIOM NAWODNIENIA - 1</option>
-    <option value={2}>POZIOM NAWODNIENIA - 2</option>
-    <option value={3}>POZIOM NAWODNIENIA - 3</option>
+        <option value={1}>POZIOM NAWODNIENIA - 1</option>
+        <option value={2}>POZIOM NAWODNIENIA - 2</option>
+        <option value={3}>POZIOM NAWODNIENIA - 3</option>
       </select>
       <input type="number" max="30" placeholder={isMobile ? 'Obciąż.' : 'Obciążanie [dni]'} value={this.state.weight} onChange={this.handleWeighting} required pattern="^\d+$" title="Wprowadź liczbę"></input>
       <input type="number" max="30" placeholder={isMobile ? 'Black.' : 'Blackout [dni]'} value={this.state.blackout} onChange={this.handleBlackout} required pattern="^\d+$" title="Wprowadź liczbę"></input>
@@ -98,10 +108,11 @@ render(){
       <button type='submit'>DODAJ</button>
       {this.state.error !== '' ? <p className="error">{this.state.error}</p> : ''}
     </form>;
-  return (
-<div id="addMicrogreens">
-{addMicrogreensForm}
-</div>);}
+    return (
+      <div id="addMicrogreens">
+        {addMicrogreensForm}
+      </div>);
+  }
 }
 
 
